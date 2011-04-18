98ffec8278973e9e1d2bdba9b843a6f3172e31b6
510598@gmail.com <510598@gmail.com@bd005844-abb5-a7c3-8b7a-2059e7dbaa47>
2011-04-18 00:48:37 +0000

    自动运行脚本

---
+++ autostart.sh
@@ -0,0 +1,22 @@
+
+dwm_dir=${HOME}/.dwm
+
+killall conky; conky -c ${dwm_dir}/conkyrc | while read -r; do 
+         
+ xsetroot -name "$REPLY"; 
+
+done &
+
+nitrogen --restore &
+
+xfce4-clipman &
+
+update-notifier &
+
+#认证代理
+/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+
+xdg-user-dirs-gtk-update &
+
+
+
